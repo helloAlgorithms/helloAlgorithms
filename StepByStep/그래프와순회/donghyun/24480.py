378fe359d849c296a9aa8992863f602232ab3992
김동현 <dungnss@gimdonghyeons-MacBook-Air.local>
2022-08-16 02:30:48 +0900

    [24480] 그래프와순회, 알고리즘 수업 - 깊이 우선 탐색 2

---
+++ StepByStep/그래프와순회/donghyun/24480.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+node, edge, start = map(int, input().split())
+graph = [[] for _ in range(node + 1)]
+for i in range(edge):
+	f, t = map(int, input().split())
+	graph[f].append(t)
+	graph[t].append(f)
+graph = [sorted(routes, reverse=True) for routes in graph]
+
+def dfs(visit):
+	dfs.visited.add(visit)
+	dfs.to_print[visit - 1] = dfs.order
+	dfs.order += 1
+	for route in graph[visit]:
+		if route not in dfs.visited:
+			dfs(route)
+dfs.visited = set()
+dfs.to_print = [0] * node
+dfs.order = 1
+dfs(start)
+print(*dfs.to_print)
