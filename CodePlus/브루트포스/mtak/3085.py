b720e59e57e36a3d7ab0b88515422b74c663e3fe
mtak <jesustark0235@gmail.com>
2022-08-17 19:52:17 +0900

    [3085] 브루트포스, 사탕게임

--- CodePlus/브루트포스/mtak/3085.py
+++ CodePlus/브루트포스/mtak/3085.py
@@ -1,24 +1,38 @@
 import sys
 input = sys.stdin.readline
 N = int(input())
-m = ""
-for _ in range(N):
-	m += input().strip()
-maxCnt = 0
-for i in range(N * N - 1):
-	for j in range(i + 1, N * N):
-		for k in range(0,N * (N - 1)):
-			for l in range(0, N):
-				t = N[l]
-				for m in range(l + 3, N, 3):
-					if N[m] != N[l] or m + 3 > N:
-						candidate = len(t)
-						if candidate >maxCnt:
-							maxCnt = candidate
-						continue
-					t += N[m]
-				p = N[l]
-				for m in range(
+M = [list(input()) for _ in range(N)];
+D = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+ans = 0
+def maxCandy():
+    rowCnt, colCnt, rowMax, colMax = 0, 0, 0, 0
+    for i in range(N):
+        for j in range(N - 1):
+            if M[i][j] == M[i][j + 1]:
+                rowCnt += 1
+            else:
+                rowCnt = 1
+            rowMax = max(rowCnt,rowMax)
+        rowCnt = 1
+    for j in range(N):
+        for i in range(N - 1):
+            if M[i][j] == M[i + 1][j]:
+                colCnt += 1
+            else:
+                colCnt = 1
+            colMax = max(colCnt, colMax)
+        colCnt = 1
+    return max(rowMax, colMax)
 
-				
-
+for i in range(N):
+	for j in range(N):
+		for k in range(4):
+			nx = i + D[k][0]
+			ny = j + D[k][1]
+			if (nx < 0 or nx >= N or ny < 0 or ny >= N):
+				continue
+			if (M[i][j] != M[nx][ny]):
+				M[i][j],M[nx][ny] = M[nx][ny],M[i][j]
+				ans = max(ans, maxCandy())
+				M[i][j],M[nx][ny] = M[nx][ny],M[i][j]
+print(ans)
