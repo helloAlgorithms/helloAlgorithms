6fa1a9a587e97e561eacf969e90ef150b6f6879a
mtak <jesustark0235@gmail.com>
2022-08-23 18:29:44 +0900

    1107 수정2

--- CodePlus/브루트포스/mtak/1107.py
+++ CodePlus/브루트포스/mtak/1107.py
@@ -4,11 +4,12 @@ n = int(input())
 ans = abs(100 - n)
 m = int(input())
 b = set(map(int, input().split()))
+h = 0
 for num in range(1000001):
-    for j in str(num):
-        if j in b:
+    for idx, j in enumerate(str(num)):
+        if int(j) in b:
             break
-        else:
+        elif idx == len(str(num)) - 1:
             ans = min(ans, len(str(num)) + abs(num - n))
             h = num
 print(ans)
