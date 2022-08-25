316dddfd065caf14abf96157deac5f2f9edaac75
mtak <jesustark0235@gmail.com>
2022-08-25 21:22:02 +0900

    [6064] 브루트포스, 카잉달력

--- CodePlus/브루트포스/mtak/1107.py
+++ CodePlus/브루트포스/mtak/1107.py
@@ -3,13 +3,13 @@ input = sys.stdin.readline
 n = int(input())
 ans = abs(100 - n)
 m = int(input())
-b = set(range(10))
-if m > 0:
-	b = b -set(map(int, input().split()))
+b = set(map(int, input().split()))
+h = 0
 for num in range(1000001):
-    for j in str(num):
-        if j not in b:
+    for idx, j in enumerate(str(num)):
+        if int(j) in b:
             break
-        else:
+        elif idx == len(str(num)) - 1:
             ans = min(ans, len(str(num)) + abs(num - n))
+            h = num
 print(ans)
