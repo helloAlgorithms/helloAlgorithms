6225c7ecbaabd7b843b4fa16082b3b1929eddbe4
mtak <jesustark0235@gmail.com>
2022-08-30 18:14:30 +0900

    [17427] 수학, 약수의 합2

--- CodePlus/수학/mtak/17427.py
+++ CodePlus/수학/mtak/17427.py
@@ -1,3 +1,19 @@
 import sys
 input = sys.stdin.readline
-test
+def f(y):
+    l = []
+    for i in range(1, int(y **(1/2)) + 1):
+        if y % i == 0:
+            l.append(i)
+            if i ** 2 != y:
+                l.append(y//i)
+    return sum(l)
+def g(x):
+    sum = 0
+    for i in range(1, x + 1):
+        sum += f(i)
+    return sum
+
+n = int(input())
+print(g(n))
+
