c9e1aef677b7aef92fc408a30c5ca2759bed77c7
김동현 <dbin67@naver.com>
2022-09-24 01:16:15 +0900

    [9252] 동적 계획법과 최단거리 역추적, LCS 2

---
+++ StepByStep/동적 계획법과 최단거리 역추적/donghyun/9252.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+a = ' ' + input().strip()
+b = ' ' + input().strip()
+dp = [[""] * len(b) for _ in range(len(a))]
+for i in range(1, len(a)):
+	for j in range(1, len(b)):
+		if a[i] == b[j]:
+			dp[i][j] = dp[i - 1][j - 1] + a[i]
+		else:
+			dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], key=len)
+print(len(dp[-1][-1]))
+print(dp[-1][-1])
