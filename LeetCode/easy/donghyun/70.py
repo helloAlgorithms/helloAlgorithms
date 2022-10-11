85eb90c34db165c9966ef2fde67567cdbdfdf6e1
김동현 <dbin67@naver.com>
2022-10-11 23:57:15 +0900

    [70] Easy, Climbing Stairs

---
+++ LeetCode/easy/donghyun/70.py
@@ -0,0 +1,6 @@
+class Solution:
+    @lru_cache(maxsize=None)
+    def climbStairs(self, n: int) -> int:
+        if n <= 1:
+            return 1
+        return self.climbStairs(n-1) + self.climbStairs(n-2)
