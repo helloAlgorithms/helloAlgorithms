06230283e79c579e2183a73d4e4cea456e8a4f14
김동현 <dbin67@naver.com>
2022-10-13 17:57:54 +0900

    [62] Medium, Unique Paths

---
+++ LeetCode/medium/donghyun/62.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp[1][1] = 1
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] += dp[i-1][j] + dp[i][j-1]
+        return dp[m][n]
