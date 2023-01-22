adc5177667dee3ee3dcef20de15f420be578fef6
mtak <jesustark0235@gmail.com>
2023-01-22 23:45:18 +0900

    [11726] 다이나믹프로그래밍, 2xn 타일링

--- CodePlus/다이나믹프로그래밍/mtak/11726.py
+++ CodePlus/다이나믹프로그래밍/mtak/11726.py
@@ -1,26 +1,11 @@
-import sys
-sys.setrecursionlimit(10000)
 n = int(input())
-n_cases = 0
-def dfs(w, h, cnt):
-	global n_cases
-	if w == n and h == 2 and cnt == n:
-		n_cases += 1
-		return 
-	if cnt == n :return 
-	for i in ['v', 'h']:
-		th = h
-		tw = w
-		if i == 'v':
-			if not (th == 0 or th == 2): continue
-			th = 2
-			tw += 1
-		else:
-			if th == 2 or tw == 0:
-				tw += 2
-				th = 1
-			else:
-				th += 1
-		dfs(tw, th ,cnt + 1)
-dfs(0, 0, 0)
-print(n_cases%1007)
+cases = [0] * (n + 1)
+cases[1] = 1
+cases[2] = 2 
+def dfs(w):
+    global cases
+    if w < 3 or cases[w] > 0 :return cases[w]
+    cases[w] = dfs(w - 1) + dfs(w - 2)
+    return cases[w]
+    
+print(dfs(n)%10007)
