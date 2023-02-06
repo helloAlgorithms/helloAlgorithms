5f08bc52d5e3076c80af61f76c0b5c01eaa56728
samin <samin@student.42seoul.kr>
2023-02-06 17:49:03 +0900

    [42576] 해시, 완주하지 못한 선수

---
+++ programmers/해시/42576_완주하지 못한 선수.py
@@ -0,0 +1,9 @@
+def solution(participant, completion):
+    answer = ''
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    
+    return participant[len(participant) - 1]
