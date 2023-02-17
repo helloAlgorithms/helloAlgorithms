0de064bb9a4e21c24bab46e582b6d6b926036a62
sohan <sohan@student.42seoul.kr>
2023-02-17 14:16:07 +0900

    chore: 파일 이동

---
+++ boj/sohan/5525.py
@@ -0,0 +1,20 @@
+N = int(input())
+M = int(input())
+S = input()
+i = 1
+j, ans = 0,0
+
+l = len(S)
+
+while i != l - 1:
+    if S[i - 1] == 'I' and S[i] == 'O' and S[i + 1] == 'I':
+        j += 1
+        if j == N:
+            j -= 1
+            ans += 1
+        i += 1
+    else:
+        j = 0
+    i += 1
+
+print(ans)
