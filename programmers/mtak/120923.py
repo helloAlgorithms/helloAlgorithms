d832fc54628d7e16da30ba148825175817198b57
Min Kyeong - Tak <jesustark0235@gmail.com>
2023-02-28 09:06:32 +0900

    Samin (#83)
    Co-authored-by: samin <samin@student.42seoul.kr>

--- programmers/mtak/120923.py
+++
@@ -1,7 +0,0 @@
-def solution(num, total):
-    answer = [0 for i in range(num)]
-    num_sum = num * (num + 1) / 2
-    sparse = (total - num_sum) / num
-    for i in range(num):
-        answer[i] = i + 1 + sparse
-    return answer
--- programmers/mtak/120992.py
+++
@@ -1,3 +0,0 @@
-def solution(M, N):
-    answer = min(M - 1 + M *(N - 1), N - 1 + N * (M - 1))
-    return answer
