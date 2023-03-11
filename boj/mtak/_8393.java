089375d87c42cf7c776234a4ab2696b2e519f6f3
mtak <jesustark0235@gmail.com>
2023-03-12 01:50:48 +0900

    [8393] 합

---
+++ boj/mtak/_8393.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class _8393 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i < t + 1; i++ ) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
