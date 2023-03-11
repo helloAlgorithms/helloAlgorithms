8583c75d3ecd1cd6bd86666d41b2536021628328
mtak <jesustark0235@gmail.com>
2023-03-12 01:48:23 +0900

    [2739] 구구단

---
+++ boj/mtak/_2739.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class _2739 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 1; i < 10; i++ ) {
+            System.out.println(t + " * " + i + " = " + (t * i));
+        }
+    }
+}
