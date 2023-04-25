df09599664c62563cfab1d78126e01fb79b5d11a
mtak <jesustark0235@gmail.com>
2023-04-26 01:45:49 +0900

    [11724] 연결 요소의 개수

---
+++ boj/mtak/11724.py
@@ -0,0 +1,31 @@
+n, m = map(int, input().split())
+graph = {}
+visited = [False] * (n + 1)
+cnt = 0
+for _ in range(m):
+	f, t = map(int, input().split())
+	tmp = graph.keys()
+	if f in tmp:
+		graph[f].append(t)
+		if t in tmp:
+			graph[t].append(f)
+		else:
+			graph[t] = [f]
+	else:
+		graph[f] = [t]
+		if t in tmp:
+			graph[t].append(f)
+		else:
+			graph[t] = [f]
+keys = list(graph.keys())
+def dfs(v):
+    if visited[v]:return
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
+for i in range(1, n + 1):
+    if not visited[i]:
+        dfs(i)
+        cnt += 1
+print(cnt)
