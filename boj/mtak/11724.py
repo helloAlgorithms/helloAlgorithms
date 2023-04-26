83342bcfcce7b41e8e4634f8ccd2d6ffa34d7eb9
mtak <jesustark0235@gmail.com>
2023-04-26 09:59:00 +0900

    [11724] 연결 요소의 개수

--- boj/mtak/11724.py
+++ boj/mtak/11724.py
@@ -1,33 +1,26 @@
+from collections import deque
 n, m = map(int, input().split())
-import sys
-sys.setrecursionlimit(10**6)
-graph = {}
+graph = [[] for _ in range(n + 1)]
 visited = [False] * (n + 1)
 cnt = 0
 for _ in range(m):
 	f, t = map(int, input().split())
-	tmp = graph.keys()
-	if f in tmp:
-		graph[f].append(t)
-		if t in tmp:
-			graph[t].append(f)
-		else:
-			graph[t] = [f]
-	else:
-		graph[f] = [t]
-		if t in tmp:
-			graph[t].append(f)
-		else:
-			graph[t] = [f]
-keys = list(graph.keys())
-def dfs(v):
-    if visited[v]:return
-    visited[v] = True
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(i)
+	graph[f].append(t)
+	graph[t].append(f)
+def bfs(v):
+    queue = deque([v])
+    while queue:
+        node = queue.popleft()
+        visited[node] = True
+        for i in graph[node]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
 for i in range(1, n + 1):
     if not visited[i]:
-        dfs(i)
+        if not graph[i]:
+            visited[i] = True
+        else:
+            bfs(i)
         cnt += 1
 print(cnt)
