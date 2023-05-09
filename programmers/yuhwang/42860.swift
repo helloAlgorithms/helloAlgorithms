23134f5b4bacc113dfde8c6e4a169730f7d39c6a
YunKi <yunki.dev@gmail.com>
2023-05-10 05:43:26 +0900

    [42860] 그리디, 조이스틱

--- programmers/yuhwang/42860.swift
+++ programmers/yuhwang/42860.swift
@@ -1,40 +1,91 @@
 import Foundation
 
 func solution(_ name:String) -> Int {
-    var answer = 0
-
-    let name = Array(name).map { String($0) }
-    var word = Array(repeating: "A", count: name.count)
-    if name == word { return 0 }
-
-    var idx = 0
-
-    while word.joined() != name {
-        if word[idx] == name[idx] {
-            var l = 0
-            var r = 0
-
-            var i = idx
-            while word[i] == name[i] {
-                i -= 1
-                if i < 0 { i = word.count - 1 }
-                l += 1
-            }
-
-            i = idx
-            while word[i] == name[i] {
-                i += 1
-                if i >= word.count { i = 0 }
-                r += 1
-            }
-
-            idx = i
-            answer += r < l ? r : l
-        }
-
-        var diff: Int = Int(Character(name[idx]).asciiValue! - Character(word[idx]).asciiValue!)
-        answer += diff <= 13 ? diff : 26 - diff
-        word[idx] = name[idx]
+    var vertical = 0
+    let notA = name.filter { $0 != "A" }.count
+    if notA == 0 { return 0 }
+    for c in name {
+        if c == "A" { continue }
+        let gap: Int = Int(c.asciiValue! - Character("A").asciiValue!)
+        vertical += gap <= 13 ? gap : 26 - gap
     }
-    return answer
+    if !name.contains("A") { return vertical + name.count - 1 }
+
+    var n = Array(name).map {String($0)}
+    var horizon = n.count - 1
+    var idx = 0
+    var move = 0
+    var c = notA
+    if n[idx] == "A" {
+        idx += 1
+        move += 1
+    }
+    while idx < n.count && c > 0 {
+        if n[idx] != "A" {
+            c -= 1
+            n[idx] = "A"
+        } else {
+            var tmp = n
+            var nota = c
+            var i = idx - 1 < 0 ? n.count - 1 : idx - 1
+            var m = move
+            while nota > 0 {
+                i -= 1
+                if i < 0 { i = n.count - 1 }
+                if tmp[i] != "A" {
+                    tmp[i] = "A"
+                    nota -= 1
+                }
+                if nota > 0 { m += 1 }
+            }
+            horizon = horizon > m ? m : horizon
+        }
+        if c == 0 {
+            horizon = horizon > move ? move : horizon
+            break
+        }
+        move += 1
+        idx += 1
+    }
+
+    n = Array(name).map { String($0) }
+    idx = n.count - 1
+    move = 1
+    c = notA
+    if n[0] != "A" {
+        c -= 1
+        n[0] = "A"
+    }
+    if n[idx] == "A" {
+        idx -= 1
+        move += 1
+    }
+    while idx > 0 {
+        if n[idx] != "A" {
+            c -= 1
+            n[idx] = "A"
+        } else {
+            var tmp = n
+            var nota = c
+            var i = idx + 1 >= n.count ? 0 : idx + 1
+            var m = move - 1
+            while nota > 0 {
+                i += 1
+                if i >= n.count { i = 0 }
+                if tmp[i] != "A" {
+                    tmp[i] = "A"
+                    nota -= 1
+                }
+                m += 1
+            }
+            horizon = horizon > m ? m : horizon
+        }
+        if c == 0 {
+            horizon = horizon > move ? move : horizon
+            break
+        }
+        move += 1
+        idx -= 1
+    }
+    return vertical + horizon
 }
